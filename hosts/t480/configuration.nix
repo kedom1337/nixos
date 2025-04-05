412c1a2d5db14ac70ca8318aa142c78e86723b44
noah <kedom@vmcall.net>
2025-04-05 20:16:40 +0200

    fix: use mkForce to overwrite default config

--- hosts/t480/configuration.nix
+++ hosts/t480/configuration.nix
@@ -1,4 +1,4 @@
-{ stateVersion, ... }:
+{ lib, stateVersion, ... }:
 {
   imports = [
     ./hardware-configuration.nix
@@ -11,5 +11,5 @@
     "/dev/disk/by-uuid/e6238f3f-e9e0-401d-9b18-370e16b4ec4e";
 
   enableLaptopServices = true;
-  boot.loader.grub.useOSProber = false;
+  boot.loader.grub.useOSProber = lib.mkForce false;
 }
