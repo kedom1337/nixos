6957443d77e2d5e5e6cb5bd107d7dbde5f6dbf6c
noah <kedom@vmcall.net>
2025-04-13 14:42:28 +0200

    feat(kitty): keybinds to go to specific tab

--- modules/home/kitty.nix
+++ modules/home/kitty.nix
@@ -1,8 +1,15 @@
+{ lib, ... }:
 {
   programs.kitty = {
     enable = true;
     settings = {
       cursor_trail = 3;
     };
+    keybindings = builtins.listToAttrs (
+      map (i: {
+        name = "alt+${toString i}";
+        value = "goto_tab ${toString i}";
+      }) (lib.range 1 5)
+    );
   };
 }
