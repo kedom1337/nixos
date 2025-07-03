edd4826e443f6be5649e3462760c691c58b6a404
noah <kedom@vmcall.net>
2025-07-04 01:23:17 +0200

    feat: improve printing config

--- modules/system/services.nix
+++ modules/system/services.nix
@@ -1,17 +1,27 @@
-{
-  config = {
-    services = {
-      printing.enable = true;
-      pulseaudio.enable = false;
-      postgresql.enable = true;
-      pipewire = {
-        enable = true;
-        alsa.enable = true;
-        alsa.support32Bit = true;
-        pulse.enable = true;
-      };
-      desktopManager.gnome.enable = true;
-      displayManager.gdm.enable = true;
+{pkgs, ...}: {
+  config.services = {
+    printing = {
+      enable = true;
+      drivers = with pkgs; [
+        gutenprint
+        hplip
+        splix
+      ];
     };
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+    };
+    pulseaudio.enable = false;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
+    desktopManager.gnome.enable = true;
+    displayManager.gdm.enable = true;
+    postgresql.enable = true;
   };
 }
