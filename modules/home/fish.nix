d3c0f3a8deaa321b5947738da3fcd95bd569bc6a
noah <kedom@vmcall.net>
2025-09-26 00:50:21 +0200

    feat(fish): add history pager

--- modules/home/fish.nix
+++ modules/home/fish.nix
@@ -4,7 +4,9 @@
     interactiveShellInit = ''
       set fish_greeting
       fish_vi_key_bindings
+
       bind -M insert \cf accept-autosuggestion
+      bind -M insert \cr history-pager
 
       abbr -a lgit "lazygit"
       abbr -a y "yazi"
