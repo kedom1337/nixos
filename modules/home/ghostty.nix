41d80c232564bdcadc4d516645c69c9d973be8e6
kedom <kedom@vmcall.net>
2025-10-30 19:42:22 +0100

    fix: ghostty announce terminfo

--- modules/home/ghostty.nix
+++ modules/home/ghostty.nix
@@ -4,6 +4,7 @@
     settings = {
       theme = "Oxocarbon";
       font-size = 11;
+      shell-integration-features = "ssh-terminfo";
       keybind = [
         "ctrl+shift+h=goto_split:left"
         "ctrl+shift+j=goto_split:down"
