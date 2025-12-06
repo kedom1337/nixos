09d2abf05c1f57f6ea3285e56ac09129fc1dc41b
kedom <kedom@vmcall.net>
2025-12-06 17:32:02 +0100

    wip: add git signing

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -2,6 +2,11 @@
   programs.git = {
     enable = true;
     lfs.enable = true;
+    signing = {
+      format = "ssh";
+      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKu4aqjfTRLu3DTl/YzgObwinrPKKFx9/yPNWvaAFEWv kedom@desktop";
+      signByDefault = true;
+    };
     settings = {
       pull.rebase = false;
       user = {
