8e1a0ff75dc1e3ead8538b96d4f13bac9f31159c
kedom <kedom@vmcall.net>
2026-01-08 01:09:38 +0100

    fix: disable vide acceleration on nvida for chromium

--- modules/home/chromium.nix
+++ modules/home/chromium.nix
@@ -1,28 +1,29 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  osConfig,
+  ...
+}: {
   programs.chromium = {
     enable = true;
     package = pkgs.ungoogled-chromium;
-    commandLineArgs = [
-      # gpu video acceleration flags
-      "--enable-features=AcceleratedVideoEncoder"
-      "--ignore-gpu-blocklist"
-      "--enable-zero-copy"
-      # ungoogled-chromium flags
-      "--fingerprinting-canvas-image-data-noise"
-      "--fingerprinting-canvas-measuretext-noise"
-      "--fingerprinting-client-rects-noise"
-      "--hide-crashed-bubble"
-      "--popups-to-tabs"
-      "--show-avatar-button=never"
-      "--disable-search-engine-collection"
-      # disable Mv3
-      "--disable-features=ExtensionManifestV2Unsupported,ExtensionManifestV2Disabled"
-      # Disable all hardware video decoding to fix green artifacts on VP8/VP9
-      # NVIDIA + VAAPI has known issues with certain codecs
-      # Note: --disable-features must appear only once (comma-separated)
-      # "--disable-accelerated-video-decode"
-      # "--disable-features=VaapiVideoDecoder,ExtensionManifestV2Unsupported,ExtensionManifestV2Disabled"
-    ];
+    commandLineArgs =
+      [
+        # ungoogled-chromium flags
+        "--fingerprinting-canvas-image-data-noise"
+        "--fingerprinting-canvas-measuretext-noise"
+        "--fingerprinting-client-rects-noise"
+        "--hide-crashed-bubble"
+        "--popups-to-tabs"
+        "--show-avatar-button=never"
+        "--disable-search-engine-collection"
+        # disable Mv3
+        "--disable-features=ExtensionManifestV2Unsupported,ExtensionManifestV2Disabled"
+      ]
+      # disable all hardware video decoding to fix artifacts on VP8/VP9 with NVIDIA
+      ++ lib.optionals osConfig.modules.system.nvidia.enable [
+        "--disable-accelerated-video-decode"
+      ];
     extensions = [
       {id = "cjpalhdlnbpafiamejdnhcphjbkeiagm";} # ublock origin
       {id = "mnjggcdmjocbbbhaepdhchncahnbgone";} # sponsor block
